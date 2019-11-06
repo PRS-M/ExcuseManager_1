16d3b68373f8b9ce59e5366d783e575dc8473be9
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2019-11-07 00:00:17 +0100

    Initial - Form Created

--- ExcuseManager_1/Form1.Designer.cs
+++ ExcuseManager_1/Form1.Designer.cs
@@ -28,13 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.excuseLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.lastUsedLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.excuseTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // excuseLabel
+            // 
+            this.excuseLabel.AutoSize = true;
+            this.excuseLabel.Location = new System.Drawing.Point(12, 15);
+            this.excuseLabel.Name = "excuseLabel";
+            this.excuseLabel.Size = new System.Drawing.Size(42, 13);
+            this.excuseLabel.TabIndex = 0;
+            this.excuseLabel.Text = "Excuse";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(12, 41);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(42, 13);
+            this.resultLabel.TabIndex = 1;
+            this.resultLabel.Text = "Results";
+            // 
+            // lastUsedLabel
+            // 
+            this.lastUsedLabel.AutoSize = true;
+            this.lastUsedLabel.Location = new System.Drawing.Point(12, 67);
+            this.lastUsedLabel.Name = "lastUsedLabel";
+            this.lastUsedLabel.Size = new System.Drawing.Size(55, 13);
+            this.lastUsedLabel.TabIndex = 2;
+            this.lastUsedLabel.Text = "Last Used";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(12, 93);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(44, 13);
+            this.dateLabel.TabIndex = 3;
+            this.dateLabel.Text = "FIll date";
+            // 
+            // excuseTextBox
+            // 
+            this.excuseTextBox.Location = new System.Drawing.Point(100, 12);
+            this.excuseTextBox.Name = "excuseTextBox";
+            this.excuseTextBox.Size = new System.Drawing.Size(250, 20);
+            this.excuseTextBox.TabIndex = 4;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(100, 38);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(250, 20);
+            this.resultTextBox.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(100, 64);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(250, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(100, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "FIll date";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(255, 121);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(369, 156);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.excuseTextBox);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.lastUsedLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.excuseLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label excuseLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label lastUsedLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.TextBox excuseTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
